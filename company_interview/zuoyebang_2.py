05b83e46ff3be8c9be84c0bb14425f48c4f99d9f
matthewhou3588 <hyfzxs@163.com>
2020-09-12 10:26:42 +0800

    Create zuoyebang_2.py

---
+++ company_interview/zuoyebang_2.py
@@ -0,0 +1,15 @@
+1. 设计模式知道哪些？观察者模式
+
+2. Kafka:数据一致性怎么保证？
+
+3. Spark take(n)的实现，怎么快速取n个数据
+
+4. Spark的调度，DagScheduler, TaskScheduler什么作用
+
+5. reduceByKey, groupByKey区别，线上groupByKey可能会出现什么？
+
+6. 宽窄依赖，任务怎么划分
+
+7. 数据倾斜，线上会有哪些表现，可能会引起什么？怎么解决
+
+8. 数据挖掘：基尼系数、信息增益、信息熵等概念，SVM高斯核
